91fa7b5bd5612bba31e9f032942236d73a959618
Ognjen Kurtic <Ognjen.Kurtic@zuehlke.com>
2019-03-14 10:53:32 +0100

    Add full API definition for the frontend

--- src/Carbonara/Controllers/CarbonaraController.cs
+++ src/Carbonara/Controllers/CarbonaraController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Carbonara.Models;
 using Carbonara.Services;
@@ -19,12 +20,41 @@ namespace Carbonara.Controllers
         }
 
         /// <summary>
-        /// Get parameters for formula of given transaction
+        /// Get years selection for approximation of miners hashrate\energy consumption
+        /// </summary>
+        /// <response code="200">List of integers representing years</response>
+        [HttpGet("MinningGearYearsSelection")]
+        public async Task<IActionResult> GetMinningGearYearsSelection()
+        {
+            var years = await Task.FromResult(new List<int> { 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 });
+            return Ok(years);
+        }
+
+        /// <summary>
+        /// Get energy standards selection for approximation of CO2 emmissions per KWH
+        /// </summary>
+        /// <response code="200">List of string representing countries with the approximate CO2 consumption</response>
+        [HttpGet("CountriesCO2EmissionSelection")]
+        public async Task<IActionResult> GetCountriesCO2EmissionSelection()
+        {
+            var countries = await Task.FromResult(new List<string> { "CN", "USA", "EU" });
+            return Ok(countries);
+        }
+
+        /// <summary>
+        /// Get the CO2 emission for a given transaction hash
         /// </summary>
         /// <param name="txHash">transaction hash</param>
+        /// <param name="minningGearYear">(Optional) Year for which the minning gear hashrate\energy consumtion approximation should be taken into account </param>
+        /// <param name="hashingAlgorithm">(Optional) Hashing alg to be used for the minning gear approximation</param>
+        /// <param name="cO2EmissionCountry">(Optional) Country for which the CO2 emission per KWH appoximation should be taken into account</param>
         /// <response code="200">Formula parameters</response>
-        [HttpGet("txHash")]
-        public async Task<IActionResult> GetFormulaParametersAsync(string txHash)
+        [HttpGet("Calculate")]
+        public async Task<IActionResult> GetFormulaParametersAsync(
+            [FromQuery(Name = "TxHash")]string txHash,
+            [FromQuery(Name = "MinningGearYear")]int? minningGearYear = null,
+            [FromQuery(Name="HashingAlgorithm")]string hashingAlgorithm = "0",
+            [FromQuery(Name = "CO2EmissionCountry")]string cO2EmissionCountry = null)
         {
             var blockParameters = await _blockParametersService.GetBlockParameters(txHash);
             var hashRate = await _networkHashRateService.GetDailyHashRateInPastAsync(blockParameters.BlockTimeInSeconds);
