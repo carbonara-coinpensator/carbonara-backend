6e79d623be81a76d3cda3f0c3efc830585bb3a5e
Achilles Portarinos <poac@zuehlke.com>
2019-03-14 11:36:09 +0100

    update project path in JsonHardwareProvider, update Json Structure

--- src/Carbonara/Providers/IJsonHardwareProvider/JsonHardwareProvider.cs
+++ src/Carbonara/Providers/IJsonHardwareProvider/JsonHardwareProvider.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Carbonara.Models.MiningHardware;
 using Carbonara.Providers;
@@ -10,15 +11,23 @@ namespace Carbonara.Services
 {
     public class JsonHardwareProvider : IJsonHardwareProvider
     {
+        private readonly string _path;
+
+        public JsonHardwareProvider()
+        {
+            _path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
         public async Task<List<MiningDevice>> GetHardwareByMiningAlgorithm(MiningAlgorithm alogrithm = MiningAlgorithm.SHA256)
         {
-            var devices = await ReadDevicesFromFile("MiningHardware.json");
+            var devices = await ReadDevicesFromFile($"{_path}/MiningHardware.json");
             return devices.Where(i => i.Algorithm == alogrithm).ToList();
         }
 
         public async Task<List<MiningDevice>> GetAll()
         {
-            var devices = await ReadDevicesFromFile("MiningHardware.json");
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            var devices = await ReadDevicesFromFile($"{_path}/MiningHardware.json");
             return devices.ToList();
         }        
 
