42c7f47917f7e8bcb0d0077c0636029d28b9c936
Sulk <sulejman.karisik@gmail.com>
2019-04-12 17:24:47 +0200

    Adds block data scrapper- not finished!

---
+++ BlockDataScrapper/BlockDataScrapper/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BlockDataScrapper
+{
+     public class Dto 
+     {
+        public string Timestamp { get; set; }
+        public string Hash { get; set; }
+     }
+
+    class Program
+    {
+        public static Uri ApiBaseAddress = new Uri("https://chain.api.btc.com/v3/");
+        public static List<RootObject> data = new List<RootObject>();
+
+        static void Main(string[] args)
+        {
+            GetData().GetAwaiter().GetResult();
+        }
+
+        private static async Task GetData()
+        {
+            HttpClient client = new HttpClient();
+
+            int year = 2015;
+            int month = 01;
+            int day = 01;
+
+            // TODO: 5 block per week, prosek 
+
+            try
+            {
+                var response = await client.GetAsync("https://chain.api.btc.com/v3/block/date/20151215");
+                var data = await response.Content.ReadAsStringAsync();
+
+                var d = JsonConvert.DeserializeObject<RootObject>(data);
+                Console.Write(d);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }           
+        }
+    }
+}
---
+++ BlockDataScrapper/BlockDataScrapper/RootData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockDataScrapper
+{
+    public class Extras
+    {
+        public string pool_name { get; set; }
+        public string pool_link { get; set; }
+    }
+
+    public class Datum
+    {
+        public int height { get; set; }
+        public int version { get; set; }
+        public string mrkl_root { get; set; }
+        public int timestamp { get; set; }
+        public int bits { get; set; }
+        public object nonce { get; set; }
+        public string hash { get; set; }
+        public string prev_block_hash { get; set; }
+        public string next_block_hash { get; set; }
+        public int size { get; set; }
+        public object pool_difficulty { get; set; }
+        public double difficulty { get; set; }
+        public int tx_count { get; set; }
+        public object reward_block { get; set; }
+        public int reward_fees { get; set; }
+        public int created_at { get; set; }
+        public int confirmations { get; set; }
+        public bool is_orphan { get; set; }
+        public int curr_max_timestamp { get; set; }
+        public bool is_sw_block { get; set; }
+        public int stripped_size { get; set; }
+        public int weight { get; set; }
+        public Extras extras { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Datum> data { get; set; }
+        public int err_no { get; set; }
+        public object err_msg { get; set; }
+    }
+}
